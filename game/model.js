3e8696aab95a002aa168e0564b15cc100171d389
Dung Phan <45093669+dung-phan@users.noreply.github.com>
2020-05-31 08:23:40 +0200

    change default value of winCheck

--- game/model.js
+++ game/model.js
@@ -13,7 +13,6 @@ const Game = db.define("game", {
   },
   winCheck: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false,
   },
   status: {
     type: Sequelize.ENUM("empty", "playing", "done"),
